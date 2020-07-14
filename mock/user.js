e77139e95f91b5ad84da898a963ec3eef4c64f40
nining <nining@gstianfu.com>
2020-07-14 14:36:10 +0800

    modify avatar

--- mock/user.js
+++ mock/user.js
@@ -12,13 +12,13 @@ const users = {
   'admin-token': {
     roles: ['admin'],
     introduction: 'I am a super administrator',
-    avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+    avatar: 'http://www2.flightclub.cn/news/uploads/allimg/180324/7-1P324125204.gif',
     name: 'Super Admin'
   },
   'editor-token': {
     roles: ['editor'],
     introduction: 'I am an editor',
-    avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+    avatar: 'http://www2.flightclub.cn/news/uploads/allimg/180324/7-1P324125204.gif',
     name: 'Normal Editor'
   }
 }
