b8391ab4692dd73d25a628a12c9989d52131dc13
nining <nining@gstianfu.com>
2020-07-21 16:02:41 +0800

    add exam

--- src/views/examine/index.vue
+++ src/views/examine/index.vue
@@ -1,30 +1,46 @@
 <template>
-<el-row>
-  <el-col :span="24"><div class="grid-content bg-purple-dark"></div></el-col>
-</el-row>
-<el-row>
-  <el-col :span="12"><div class="grid-content bg-purple"></div></el-col>
-  <el-col :span="12"><div class="grid-content bg-purple-light"></div></el-col>
-</el-row>
-<el-row>
-  <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
-  <el-col :span="8"><div class="grid-content bg-purple-light"></div></el-col>
-  <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
-</el-row>
-<el-row>
-  <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-  <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
-  <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-  <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
-</el-row>
-<el-row>
-  <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-  <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
-  <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-  <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
-  <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-  <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
-</el-row>
+<div class="exam">
+        <el-row>
+        <el-col :span="15" :offset="2">
+            <el-row :gutter="30">
+                <el-col :span="8">
+                    <el-card shadow="hover">
+                    <div class="inner">
+                        <div>第一模块</div>
+                        <ul>
+                            <li>第一模块：Python从入门到放弃</li>
+                            <li>第一模块：Python从入门到放弃</li>
+                            <li>第一模块：Python从入门到放弃</li>
+                        </ul>
+                    </div>
+                    </el-card>
+                </el-col>
+                
+                <el-col :span="8">
+                    <el-card shadow="hover">
+                        <div class="inner">
+                            第二模块<br>
+                            第二模块：Python从入门到放弃
+                            第二模块：Python从入门到放弃
+                        </div>
+                    </el-card>
+                </el-col>
+                
+                <el-col :span="8">
+                    <el-card shadow="hover">
+                        <div class="inner">
+                            第三模块<br>
+                            第三模块：Python从入门到放弃
+                            第三模块：Python从入门到放弃
+                        </div>
+                    </el-card>
+                </el-col>
+            </el-row>
+
+        </el-col>
+    </el-row>
+
+</div>
 </template>
 
 <script>
@@ -32,31 +48,15 @@
 </script>
 
 <style scoped>
-  .el-row {
-    margin-bottom: 20px;
-    &:last-child {
-      margin-bottom: 0;
-    }
-  }
-  .el-col {
-    border-radius: 4px;
-  }
-  .bg-purple-dark {
-    background: #99a9bf;
-  }
-  .bg-purple {
-    background: #d3dce6;
-  }
-  .bg-purple-light {
-    background: #e5e9f2;
-  }
-  .grid-content {
-    border-radius: 4px;
-    min-height: 36px;
-  }
-  .row-bg {
-    padding: 10px 0;
-    background-color: #f9fafc;
-  }
+.exam{
+    margin-top: 50px;
+}
+.inner{
+    height: 120px;
+}
+.inner div{
+    margin: auto;
+    text-align: center;
+}
 </style>
 
